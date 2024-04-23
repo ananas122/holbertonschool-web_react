e7c982629ec734e0a543b6bcc67a8093b6fcddf6
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-04-23 19:37:54 +0200

    task 11

--- react_intro/task_5/dashboard/src/Notifications/Notifications.js
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Notifications.css";
-import { getLatestNotification } from "./utils";
+import { getLatestNotification } from "../utils/utils";
 import closeIcon from "./close-icon.png";
 
 const Notifications = () => {
@@ -14,11 +14,11 @@ const Notifications = () => {
                 style={{
                     position: "absolute",
                     right: '20px',
-                    top: '20px', 
+                    top: '20px',
                     background: 'transparent',
                     border: 'none',
                     height: '20px',
-                    width: '20px', 
+                    width: '20px',
                 }}
                 aria-label="Close"
                 onClick={handleCloseClick}
