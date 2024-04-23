8ed8df230737a48f015a643ec0346d4f3d16b1fb
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-04-23 19:55:12 +0200

    Update

--- react_intro/task_5/dashboard/src/App/App.js
+++ react_intro/task_5/dashboard/src/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import logo from '../assets/logo.png';
+import logo from '../assets/holberton-logo.jpg';
 import { getFooterCopy, getFullYear } from '../utils/utils';
 
 function App() {
