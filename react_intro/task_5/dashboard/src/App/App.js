a6b632518a32641fd376a1dd19f3b8f21ba8589c
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-04-24 21:34:31 +0200

    task 10

--- react_intro/task_5/dashboard/src/App/App.js
+++ react_intro/task_5/dashboard/src/App/App.js
@@ -1,32 +1,30 @@
-import React from 'react';
 import './App.css';
-import logo from '../assets/holberton-logo.jpg';
-import { getFooterCopy, getFullYear } from '../utils/utils';
+import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <div className="App-header">
-        <img
-          src={logo}
-          // src="./assets/holberton_logo.jpg"
-          alt="logo"
-        />
+    <div className='App'>
+      <header className='App-header'>
+        <img src="/logo.png" className="App-logo" alt="Holberton logo" />
         <h1>School dashboard</h1>
-      </div>
-      <div className="App-body">
-        <p>
-          Login to access the full dashboard
-        </p>
+      </header>
+      <body className='App-body'>
         <label htmlFor="email">Email: </label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="password">Password: </label>
-        <input type="password" id="password" name="password" />
+        <input type="email" id="email" />
+        <span style={{ margin: '0 10px' }}></span> {/* espace horizontal */}
+        <label htmlFor="psw">Password: </label>
+        <input type="password" id="psw" />
+        <span style={{ margin: '0 10px' }}></span> {/* espace horizontal */}
         <button>OK</button>
-      </div>
-      <div className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
-      </div>
+        <p>Login to access the full dashboard</p>
+
+      </body>
+      <footer className='App-footer'>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+        {/* <p>{`Copyright ${getFullYear()} - ${getFooterCopy(true)}`}</p> */}
+      </footer>
     </div>
   );
 }
